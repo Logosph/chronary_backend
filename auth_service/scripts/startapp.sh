b65eabd965400dc56c0b6e9e362b6ec1d2dd2a24
logosph <zaklyakov2004@ya.ru>
2025-05-21 20:22:45 +0300

    bug fix

--- auth_service/scripts/startapp.sh
+++ auth_service/scripts/startapp.sh
@@ -4,4 +4,4 @@ echo 'RUNNING MIGRATIONS:'
 alembic upgrade head
 echo 'MIGRATIONS RAN. EXITING'
 
-uvicorn app.main:app --port 8001 --host 0.0.0.0 --workers 4
+uvicorn app.main:app --port 8001 --host 0.0.0.0 --workers 2
--- time_tracker_service/scripts/startapp.sh
+++ time_tracker_service/scripts/startapp.sh
@@ -4,4 +4,4 @@ echo 'RUNNING MIGRATIONS:'
 alembic upgrade head
 echo 'MIGRATIONS RAN. EXITING'
 
-uvicorn app.main:app --port 8002 --host 0.0.0.0 --workers 4
+uvicorn app.main:app --port 8002 --host 0.0.0.0 --workers 2
